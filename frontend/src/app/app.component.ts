3ec53795a57dc383d0bca03bcd125f5119cefe1e
Peter <peter.muerz@gmail.com>
2022-01-14 11:10:30 +0100

    Sidenav close onClick

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,4 +10,11 @@ export class AppComponent {
   title = 'frontend';
   constructor(public userService: UserService){
   }
+  menuOnClick(){
+    const menu = document.getElementById('menu')
+    menu?.classList.toggle('opened');
+    // @ts-ignore
+    menu?.setAttribute('aria-expanded', menu?.classList.contains('opened'));
+  }
+
 }
