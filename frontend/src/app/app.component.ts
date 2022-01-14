793835245193b2070a380f157d81c3a2f150932c
Peter <peter.muerz@gmail.com>
2022-01-14 12:15:10 +0100

    Refactor

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,32 +10,13 @@ export class AppComponent {
   title = 'frontend';
 
   constructor(public userService: UserService){
-    //document.getElementById("content")?.addEventListener("scroll", this.header());
-
-    console.log(window.onscroll);
 
   }
-  ngOnInit(){
 
-
-  }
   menuOnClick(){
     const menu = document.getElementById('menu')
     menu?.classList.toggle('opened');
     // @ts-ignore
     menu?.setAttribute('aria-expanded', menu?.classList.contains('opened'));
   }
-  header() {
-    const header = document.getElementById('header')
-    const sticky = header?.offsetTop;
-    if (sticky != undefined && window.pageYOffset > sticky) {
-      header?.classList.add("sticky");
-    } else {
-      header?.classList.remove("sticky");
-    }
-  }
-
-
-
-
 }
