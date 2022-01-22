2813276913208230906abbbe4d68d1c8c126b087
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2022-01-22 17:20:27 +0100

    added columns

--- frontend/src/app/friends/friends.component.ts
+++ frontend/src/app/friends/friends.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User, UserService} from "../services/user.service";
+import {FormControl} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
 
 
 @Component({
@@ -9,11 +11,30 @@ import {User, UserService} from "../services/user.service";
 })
 export class FriendsComponent implements OnInit {
 
-  users: User[] = []
-  constructor(private userService: UserService) { }
+  searchFilterFormControl = new FormControl('');
+  users: User[] = [];
+  filteredUsers: User[] = [];
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(users => this.users = users)
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+      this.searchFilter(this.searchFilterFormControl.value);
+    } )
+
+    this.searchFilterFormControl.valueChanges.subscribe(value => this.searchFilter(value));
+    this.route.paramMap.subscribe(params => this.searchFilterFormControl.setValue(params.get('')) );
+
+
+  }
+  searchFilter(filterValue: string) {
+    this.filteredUsers = this.users.filter(u => {
+      return !filterValue || u.username.toLowerCase().includes(filterValue.toLowerCase())
+    });
+
   }
 
 }
